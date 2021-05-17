331bc6e3ec6aca2c6c90ba1a88a37e33e828c8ea
pyotoru <wawrzyniak.peter@gmail.com>
2021-05-17 22:52:28 +0900

    added some quotes

--- quotes.js
+++ quotes.js
@@ -27,6 +27,46 @@ const quotes = [
 		quote: "The most damaging phrase in the language is.. it's always been done this way",
 		author: "Grace Hopper",
 	},
+
+	{
+		quote: "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+		author: "Martin Fowler",
+	},
+
+	{
+		quote: "First, solve the problem. Then, write the code.",
+		author: "John Johnson",
+	},
+
+	{
+		quote: "Experience is the name everyone gives to their mistakes.",
+		author: "Oscar Wilde",
+	},
+
+	{
+		quote: "In order to be irreplaceable, one must always be different",
+		author: "Coco Chanel",
+	},
+
+	{
+		quote: "Java is to JavaScript what car is to Carpet.",
+		author: "Chris Heilmann",
+	},
+
+	{
+		quote: "Knowledge is power.",
+		author: "Francis Bacon",
+	},
+
+	{
+		quote: "Sometimes it pays to stay in bed on Monday, rather than spending the rest of the week debugging Monday’s code.",
+		author: "Dan Salomon",
+	},
+
+	{
+		quote: "Code is like humor. When you have to explain it, it’s bad.",
+		author: "Cory House",
+	},
 ];
 
 var randomQuote = [];
