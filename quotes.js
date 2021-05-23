f95fde38daa0838c64a165b0bf70f7a4fbf997a9
pyotoru <wawrzyniak.peter@gmail.com>
2021-05-23 13:59:58 +0900

    changing tweet

--- quotes.js
+++ quotes.js
@@ -84,10 +84,16 @@ function getRandomQuote() {
 $("button").addClass("animate__animated animate__bounce");
 $("#tweet-quote").addClass("animate__animated animate__bounce");
 $("#quote-box-quote").fadeIn("slow");
-$("#new-quote").click(function () {
+$("#new-quote").click(function fadeOut() {
 	$("#quote-box-quote").fadeOut("slow");
 });
 
-$("#new-quote").click(function () {
+$("#new-quote").click(function fadeIn() {
 	$("#quote-box-quote").fadeIn("slow");
 });
+
+$("#tweet-quote").attr(
+	"href",
+	"https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=" +
+		encodeURIComponent('"' + randomQuote + '" ' + randomQuoteAuthor)
+);
