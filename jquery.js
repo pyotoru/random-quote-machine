5383747d28e01a34020bfbeb49a12f4e118e4b5d
pyotoru <wawrzyniak.peter@gmail.com>
2021-07-11 09:05:19 +0900

    small fix

--- jquery.js
+++ jquery.js
@@ -2,8 +2,6 @@ $(document).ready(function () {
 	$("button").addClass("animate__animated animate__bounce");
 	$("#quote-box-quote").fadeIn("slow");
 
-	// to be improved later on
-
 	$("#new-quote").click(function () {
 		$("#quote-box-quote")
 			.fadeOut("slow", () => getQuote())
